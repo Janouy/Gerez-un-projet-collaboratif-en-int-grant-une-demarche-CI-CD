06220e34138501f763fcf16770c74c4dc989dad4
Jeanne Boutry <jeanne.boutry@hotmail.fr>
2026-01-09 14:51:08 +0100

    remove karma update

--- front/karma.conf.js
+++ front/karma.conf.js
@@ -27,11 +27,7 @@ module.exports = function (config) {
     coverageReporter: {
       dir: require("path").join(__dirname, "./coverage/bobapp"),
       subdir: ".",
-      reporters: [
-        { type: "html" },
-        { type: "lcovonly", file: "lcov.info" },
-        { type: "text-summary" },
-      ],
+      reporters: [{ type: "html" }, { type: "text-summary" }],
     },
     reporters: ["progress", "kjhtml"],
     port: 9876,
